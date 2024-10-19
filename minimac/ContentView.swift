b52225edd9574c550490eea89ab9473bf9a60e89
Audra1 <lucasaudra1@gmail.com>
2024-10-18 21:10:06 -0300

    Initial Commit

---
+++ minimac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  minimac
+//
+//  Created by Lucas Audra on 10/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ minimac/minimacApp.swift
@@ -0,0 +1,17 @@
+//
+//  minimacApp.swift
+//  minimac
+//
+//  Created by Lucas Audra on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct minimacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
